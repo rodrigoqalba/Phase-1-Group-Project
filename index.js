f325cfec261c6d48fa0b193b066545cf0aac46dd
Rodrigo Alba <rodrigoqalba@gmail.com>
2021-10-04 17:25:14 -0400

    pushed

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-const baseURL = fetch ('https://superheroapi.com/api/1856344077860375')
-    .then (response => response.json())
-    .then(data => console.log(data))
+// const baseURL = 'https://superheroapi.com/api/1856344077860375/';
+
+// fetch (baseURL)
+//     .then (response => response.json())
+//     .then(data => console.log(data))
+
+
+// api url
+const api_url = 
+      "https://superheroapi.com/api/1856344077860375/";
+  
+// Defining async function
+async function getapi(url) {
+    
+    // Storing response
+    const response = await fetch(url);
+    
+    // Storing data in form of JSON
+    var data = await response.json();
+    console.log(data);
+    if (response) {
+        hideloader();
+    }
+    show(data);
+}
+// Calling that async function
+getapi(api_url);
